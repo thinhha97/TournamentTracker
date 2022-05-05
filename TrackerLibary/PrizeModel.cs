a86e715bea4e810c8c9c7e151cb2c35f2f637878
Thinh Ha <havanthinh97@gmail.com>
2022-05-05 20:41:01 +0700

    Add 'TrackerLibrary' Class Library.

---
+++ TrackerLibary/PrizeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibary
+{
+    public class PrizeModel
+    {
+        public int PlaceNumber { get; set; }
+        public string PlaceName { get; set; }
+        public decimal PriceAmount { get; set; }
+        public double PricePercentage { get; set; }
+    }
+}
