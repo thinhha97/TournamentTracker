3cdddeaf25b07df0caad70802d559c3dec573ab7
Thinh Ha <havanthinh97@gmail.com>
2022-05-19 01:20:40 +0700

    Add Table Teams.

---
+++ TrackerDatabase/Tables/Teams.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Teams]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1000, 1), 
+    [TeamNames] NVARCHAR(100) NOT NULL, 
+    [CreatedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
+    [ModifiedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE()
+)
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_Teams_Updated]
+    ON [dbo].[Teams]
+    FOR UPDATE
+    AS
+    BEGIN
+        SET NoCount ON;
+        UPDATE [dbo].[Teams]
+        SET [ModifiedAt] = GETUTCDATE()
+        WHERE Id IN (SELECT Id FROM inserted);
+    END
