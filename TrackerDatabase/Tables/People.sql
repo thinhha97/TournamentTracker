5b141e623d24bf2569e3ead04c5f40abd7afb787
Thinh Ha <havanthinh97@gmail.com>
2022-05-19 00:58:55 +0700

    Add People table.

---
+++ TrackerDatabase/Tables/People.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[People]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(20) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(50) NOT NULL, 
+    [PhoneNumber] NCHAR(10) NOT NULL, 
+    [CreatedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
+    [ModifiedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE()
+)
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_People_Updated]
+    ON [dbo].[People]
+    FOR UPDATE
+    AS
+    BEGIN
+        SET NoCount ON;
+        UPDATE [dbo].[People]
+        SET [ModifiedAt] = GETUTCDATE()
+        WHERE Id IN (SELECT DISTINCT Id FROM inserted);
+    END
