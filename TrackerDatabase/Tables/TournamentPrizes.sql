3028def800991d779589f2b45a7139064f356560
Thinh Ha <havanthinh97@gmail.com>
2022-05-19 02:01:12 +0700

    Add Table TournamentPrizes.

---
+++ TrackerDatabase/Tables/TournamentPrizes.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[TournamentPrizes]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1000, 1), 
+    [TournamentId] INT NOT NULL, 
+    [PrizeId] INT NOT NULL, 
+    [CreatedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
+    [ModifiedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
+    CONSTRAINT [FK_TournamentPrizes_Tournaments] FOREIGN KEY ([TournamentId]) REFERENCES [Tournaments](Id) ON DELETE CASCADE, 
+    CONSTRAINT [FK_TournamentPrizes_Prizes] FOREIGN KEY ([PrizeId]) REFERENCES [Prizes]([Id]) ON DELETE CASCADE
+)
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_TournamentPrizes_Updated]
+    ON [dbo].[TournamentPrizes]
+    FOR UPDATE
+    AS
+    BEGIN
+        SET NoCount ON;
+        UPDATE [dbo].[TournamentPrizes]
+        SET [ModifiedAt] = GETUTCDATE()
+        WHERE Id IN (SELECT DISTINCT Id FROM inserted);
+    END
