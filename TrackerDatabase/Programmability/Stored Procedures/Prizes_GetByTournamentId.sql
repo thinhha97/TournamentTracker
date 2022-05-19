0737430b33ccda4ab5116408aa1514da65dcaf2a
Thinh Ha <havanthinh97@gmail.com>
2022-05-19 17:22:08 +0700

    Create sp 'Prizes_GetByTournamentId'.

---
+++ TrackerDatabase/Programmability/Stored Procedures/Prizes_GetByTournamentId.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Prizes_GetByTournamentId]
+	@TournamentId int
+AS
+BEGIN
+	SET NoCOUNT ON;
+	SELECT p.*
+	FROM [dbo].[Prizes] p
+	INNER JOIN [dbo].[TournamentPrizes] t ON p.Id = t.PrizeId
+	WHERE t.TournamentId = @TournamentId;
+END
