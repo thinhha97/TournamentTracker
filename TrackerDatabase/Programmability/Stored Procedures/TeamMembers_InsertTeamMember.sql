82e99673ba2970909ddba6878d2de5c1612a399d
Thinh Ha <havanthinh97@gmail.com>
2022-06-02 00:13:25 +0700

    Create 'TeamMembers_InsertTeamMember' sp.

---
+++ TrackerDatabase/Programmability/Stored Procedures/TeamMembers_InsertTeamMember.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[TeamMembers_InsertTeamMember]
+	@TeamId int,
+	@PersonId int,
+	@id int = 0 output
+AS
+BEGIN
+	SET NoCOUNT ON;
+	INSERT INTO [dbo].[TeamMembers](TeamId, PersonId)
+	VALUES (@TeamId, @PersonId);
+
+	SELECT @id = SCOPE_IDENTITY();
+END
