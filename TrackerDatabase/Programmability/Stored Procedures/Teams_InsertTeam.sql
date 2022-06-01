09feebf6d3e64c8169ed3e0e281d75b13fe7fc76
Thinh Ha <havanthinh97@gmail.com>
2022-06-02 00:10:10 +0700

    Create 'Teams_InsertTeam' sp.

---
+++ TrackerDatabase/Programmability/Stored Procedures/Teams_InsertTeam.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Teams_InsertTeam]
+	@TeamName nvarchar(100),
+	@id int = 0 output
+AS
+BEGIN
+	SET NoCOUNT ON;
+	INSERT INTO [dbo].Teams(TeamName)
+	VALUES (@TeamName);
+
+	SELECT @id = SCOPE_IDENTITY();
+END
