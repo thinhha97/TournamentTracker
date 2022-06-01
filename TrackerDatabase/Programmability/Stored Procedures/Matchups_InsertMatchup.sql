a5a488bafd4c71f365dcafb23507b984be27ff71
Thinh Ha <havanthinh97@gmail.com>
2022-06-02 05:25:23 +0700

    .

---
+++ TrackerDatabase/Programmability/Stored Procedures/Matchups_InsertMatchup.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Matchups_InsertMatchup]
+	@TournamentId INT,
+	@WinnerId INT,
+	@MatchupRound INT,
+	@id INT = 0 OUTPUT
+AS
+BEGIN
+	SET NoCOUNT ON;
+	INSERT INTO [dbo].[Matchups]( TournamentId, WinnerId, MatchupRound)
+	VALUES (@TournamentId, @WinnerId, @MatchupRound);
+
+	SELECT @id = SCOPE_IDENTITY();
+END
--- TrackerDatabase/Tables/Matchups.sql
+++ TrackerDatabase/Tables/Matchups.sql
@@ -1,11 +1,13 @@
 ﻿CREATE TABLE [dbo].[Matchups]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1000, 1), 
-    [WinnerId] INT NOT NULL, 
+    [TournamentId] INT NOT NULL,
+    [WinnerId] INT NULL, 
     [MatchupRound] INT NOT NULL, 
     [CreatedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
     [ModifiedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
-    CONSTRAINT [FK_Matchups_Teams] FOREIGN KEY ([WinnerId]) REFERENCES [Teams]([Id]) ON DELETE CASCADE
+    CONSTRAINT [FK_Matchups_Teams] FOREIGN KEY ([WinnerId]) REFERENCES [Teams]([Id]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_Matchups_Tournament] FOREIGN KEY ([TournamentId]) REFERENCES [Tournament]([Id])
 )
 
 GO
