1d3e717213b3e69beeacb8d4dce8e79d410eb8c2
Thinh Ha <havanthinh97@gmail.com>
2022-06-01 22:23:07 +0700

    Create 'People_InsertPerson' sp.

---
+++ TrackerDatabase/Programmability/Stored Procedures/People_InsertPerson.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[People_InsertPerson]
+	@FirstName nvarchar(20),
+	@LastName nvarchar(50),
+	@EmailAddress nvarchar(100),
+	@CellPhoneNumber varchar(10),
+	@id int = 0 output
+AS
+BEGIN
+	SET NoCOUNT ON;
+	INSERT INTO [dbo].[People](FirstName, LastName, EmailAddress, PhoneNumber)
+	VALUES (@FirstName, @LastName, @EmailAddress, @CellPhoneNumber);
+
+	SELECT @id = SCOPE_IDENTITY();
+END
