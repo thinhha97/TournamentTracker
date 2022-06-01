2cfc9adb2ee9422e651da1f06d3384788631870d
Thinh Ha <havanthinh97@gmail.com>
2022-06-02 01:32:31 +0700

    Create 'GetTeamMembersByTeamId' sp.

---
+++ TrackerDatabase/Programmability/Stored Procedures/GetTeamMembersByTeamId.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetTeamMembersByTeamId]
+	@TeamId int
+AS
+BEGIN
+	SET NoCOUNT ON;
+	SELECT p.Id, p.FirstName, p.LastName, p.EmailAddress, p.PhoneNumber
+	FROM [dbo].[TeamMembers] tm
+	INNER JOIN [dbo].[People] p
+	ON tm.PersonId = p.Id
+	WHERE tm.TeamId = @TeamId;
+END
