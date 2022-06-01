c3e0502976ef089a75a7be629b77c5d7997dc96d
Thinh Ha <havanthinh97@gmail.com>
2022-06-01 21:09:41 +0700

    Add 'Prizes_InsertPrize' sp.

---
+++ TrackerDatabase/Programmability/Stored Procedures/Prizes_InsertPrize.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Prizes_InsertPrize]
+	@PlaceNumber  int,
+	@PlaceName nvarchar(50),
+	@PrizeAmount decimal(18,2),
+	@PrizePercentage float
+AS
+BEGIN
+	SET NoCOUNT ON;
+	INSERT INTO [dbo].[Prizes](PlaceNumber, PlaceName, PrizeAmount, PrizePercentage) 
+	VALUES (@PlaceNumber, @PlaceName, @PrizeAmount, @PrizePercentage);
+END
