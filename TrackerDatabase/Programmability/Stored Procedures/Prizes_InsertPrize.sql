19caaa5331b298c8d08d4775eeae79301580a3fd
Thinh Ha <havanthinh97@gmail.com>
2022-06-01 21:49:19 +0700

    Declare output variable.

--- TrackerDatabase/Programmability/Stored Procedures/Prizes_InsertPrize.sql
+++ TrackerDatabase/Programmability/Stored Procedures/Prizes_InsertPrize.sql
@@ -2,10 +2,13 @@
 	@PlaceNumber  int,
 	@PlaceName nvarchar(50),
 	@PrizeAmount decimal(18,2),
-	@PrizePercentage float
+	@PrizePercentage float,
+	@id int = 0 output
 AS
 BEGIN
 	SET NoCOUNT ON;
 	INSERT INTO [dbo].[Prizes](PlaceNumber, PlaceName, PrizeAmount, PrizePercentage) 
 	VALUES (@PlaceNumber, @PlaceName, @PrizeAmount, @PrizePercentage);
+
+	SELECT @id = SCOPE_IDENTITY();
 END
