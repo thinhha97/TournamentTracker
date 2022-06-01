e61b1cf0078f6ad53a4bc6d2e8650cae815807fc
Thinh Ha <havanthinh97@gmail.com>
2022-06-02 03:29:56 +0700

    Create 'Tournaments_InsertTournament' sp.

---
+++ TrackerDatabase/Programmability/Stored Procedures/Tournaments_InsertTournament.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[Tournaments_InsertTournament]
+	@TournamentName NVARCHAR(200),
+	@EntryFee DECIMAL(18,2),
+	@id INT = 0 OUTPUT
+AS
+BEGIN
+	SET NoCOUNT ON;
+	INSERT INTO [dbo].[Tournaments](TournamentName, EntryFee)
+	VALUES (@TournamentName, @EntryFee);
+
+	SELECT @id = SCOPE_IDENTITY();
+END
